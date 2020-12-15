3d43edfede63e83e5cc24bd3620beec579e660ad
Arno van Boven <arnovanboven@outlook.com>
2020-12-15 01:51:19 +0100

    change class name

---
+++ Get-CmcFilterQualifiers.cs
@@ -0,0 +1,11 @@
+using System.Management.Automation;
+using Vovin.CmcLibNet.Database;
+
+namespace PSCommenceModules
+{
+
+    [Cmdlet(VerbsCommon.Get, "CmcFilterQualifier")]
+    public class GetCmcFilterQualifier : PSCmdlet
+    {
+    }
+}
