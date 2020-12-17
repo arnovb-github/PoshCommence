e7dcbcd4f1c5d217c401c13ec89c3a8ac27925d2
Arno van Boven <arnovanboven@outlook.com>
2020-12-17 01:14:52 +0100

    Add Get-CmcConnectedItemCount cmdlet

--- PSGetCmcConnectedItemCount.cs
+++ PSGetCmcConnectedItemCount.cs
@@ -1,11 +1,13 @@
+using System;
 using System.Collections.Generic;
 using System.Management.Automation;
+using Vovin.CmcLibNet;
 
 namespace PSCommenceModules
 {
 
     [Cmdlet(VerbsCommon.Get, "CmcConnectedItemCount")]
-    public class CmcConnectedItemCount : PSCmdlet
+    public class GetCmcConnectedItemCount : PSCmdlet
     {
         private string fromCategory;
         [Parameter(Position = 0, Mandatory = true)]
@@ -39,21 +41,30 @@ namespace PSCommenceModules
             get { return fromItem; }
             set { fromItem = value; }
         }
-        
+        // Vovin.CmcLibNet calls that use a DDE call swallow all errors
+        // so this may return nothing       
         protected override void ProcessRecord()
         {
             var db = new Vovin.CmcLibNet.Database.CommenceDatabase();
+            string result = db.ViewCategory(fromCategory); // if fromCategory is not found, nothing happens
+            if (result.ToLower() != "ok") {
+                WriteError(new ErrorRecord(new Vovin.CmcLibNet.CommenceDDEException(result),
+                    "CategoryNotFound",
+                    ErrorCategory.InvalidResult,
+                    db));
+                return;
+            }
             string clarifyState = db.ClarifyItemNames();
-            int connectedItemCount;
             try {
+                int connectedItemCount = -1;
+                // no item supplied, process entire category
                 if (string.IsNullOrEmpty(fromItem))
                 {
                     int numItems = db.GetItemCount(fromCategory);
+                    List<string> itemNames = db.GetItemNames(fromCategory);
+                    db.ClarifyItemNames("TRUE");
                     for (int i = 1; i <= numItems; i++) // DDE-call indexes in Commence are 1-based
                     {
-                        db.ViewCategory(fromCategory);
-                        db.ClarifyItemNames("TRUE");
-                        List<string> itemNames = db.GetItemNames(fromCategory);
                         // since this not rely on string values, it should be reliable
                         connectedItemCount = db.ViewConnectedCount(i, connectionName, toCategory);
                         WriteObject(new
@@ -84,6 +95,23 @@ namespace PSCommenceModules
                         Count = connectedItemCount
                     },
                         false);
+
+                }
+                if (connectedItemCount == -1) {
+                    WriteError(new ErrorRecord(new Vovin.CmcLibNet.CommenceDDEException("Vovin.CmcLibNet was unable to get a valid result from Commence."),
+                        "CommenceDDEError",
+                        ErrorCategory.InvalidArgument,
+                        db));
+
+                    WriteVerbose("A count of -1 means an error occurred while trying to receive the count from Commence.\n"
+                        + "This is likely caused by one or more of the arguments being invalid.\n"
+                        + "Note that connection- and viewnames in Commence are case-sensitive!\n\n"
+                        + "Another cause may be that the arguments contain characters that break DDE commands.\n\n"
+                        + "In that case you options are:\n"
+                        + "-talk to the [Vovin.CmcLibNet] library directly,\n"
+                        + " with the 'EncodeDDEArguments' property of [Vovin.CmcLibNet.Database.CommenceDatabase] set to 'false',\n"
+                        + "-talk to Commence natively.");
+                    return;
                 }
             }
             finally
