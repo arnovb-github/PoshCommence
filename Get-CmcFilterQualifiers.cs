d5371aabe30dee5130620d465fb551180a0a9d85
Arno van Boven <arnovanboven@outlook.com>
2020-12-17 01:14:17 +0100

    Add Show-CmcFilterQualifierscmdlet

--- Get-CmcFilterQualifiers.cs
+++ Get-CmcFilterQualifiers.cs
@@ -1,11 +1,26 @@
+using System;
 using System.Management.Automation;
+using System.Text;
 using Vovin.CmcLibNet.Database;
 
 namespace PSCommenceModules
 {
 
-    [Cmdlet(VerbsCommon.Get, "CmcFilterQualifier")]
-    public class GetCmcFilterQualifier : PSCmdlet
+    [Cmdlet(VerbsCommon.Show, "CmcFilterQualifiers")]
+    public class ShowCmcFilterQualifiers : PSCmdlet
     {
+
+        protected override void ProcessRecord()
+        {
+            
+            foreach (FilterQualifier q in (FilterQualifier[]) Enum.GetValues(typeof(FilterQualifier)))
+            {
+                StringBuilder sb = new StringBuilder("[");
+                sb.Append(typeof(FilterQualifier).Namespace);
+                sb.Append("]::");
+                sb.Append(q.ToString());
+                WriteObject(new { Qualifier = sb.ToString(), Value = (int)q });
+            }
+        }
     }
 }
