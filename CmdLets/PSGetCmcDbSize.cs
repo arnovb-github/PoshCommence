366841e4c6d1b83d5ab10e00bb16909eb007a862
Arno van Boven <arnovanboven@outlook.com>
2021-01-09 01:15:11 +0100

    add Get-CmcDbSize

--- CmdLets/PSGetCmcDbSize.cs
+++ CmdLets/PSGetCmcDbSize.cs
@@ -11,16 +11,14 @@ namespace PSCommenceModules.CmdLets
     {
         protected override void ProcessRecord()
         {
-            WriteObject(GetDbSize());
-        }
-
-        private long GetDbSize() 
-        {
             using (ICommenceDatabase db = new CommenceDatabase())
             {
                 //return db.GetDatabaseSchema().Size; // this is too complicated. It does a ton of unneeded DDE
-                return Directory.GetFiles(db.Path, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length));
+                long size = Directory.GetFiles(db.Path, "*", SearchOption.AllDirectories).Sum(t => (new FileInfo(t).Length));
+                WriteObject(size);
+                WriteVerbose($"The total size of all files in database path {db.Path} is {size} bytes");
             }
         }
+
     }
 }
