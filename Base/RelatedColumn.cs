5b175b39e30453e9c1c3f4adc113c0522cfc0cbf
Arno van Boven <arnovanboven@outlook.com>
2021-02-12 23:56:47 +0100

    Add Get-CmcRelatedColumn

--- Base/RelatedColumn.cs
+++ Base/RelatedColumn.cs
@@ -1,4 +1,8 @@
-namespace PoshCommence.Base
+/* Notice that the namespace is PoshCommence and not PoshCommence.Base
+    This is because it would be confusing to create the type in Powershell
+    New-Object -TypeName poshcommence.base.relatedcolumn <- not intuitive
+*/
+namespace PoshCommence
 {
     public class RelatedColumn
     {
---
+++ CmdLets/PSGetCmcRelatedColumn.cs
@@ -0,0 +1,38 @@
+using System.Management.Automation;
+
+namespace PoshCommence.CmdLets
+{
+    [Cmdlet(VerbsCommon.Get, "CmcRelatedColumn")]
+    public class GetCmcRelatedColumn : PSCmdlet
+    {
+        private string connectionName;
+        [Parameter(Position = 0, Mandatory = true)]
+        public string ConnectionName
+        {
+            get { return connectionName; }
+            set { connectionName = value; }
+        }
+
+        private string toCategory;
+        [Parameter(Position = 1, Mandatory = true)]
+        public string ToCategory
+        {
+            get { return toCategory; }
+            set { toCategory = value; }
+        }
+        
+        private string fieldName;
+        [Parameter(Position = 2, Mandatory = true)]
+        public string FieldName
+        {
+            get { return fieldName; }
+            set { fieldName = value; }
+        }
+
+        protected override void ProcessRecord()
+        {
+            var rc = new RelatedColumn(connectionName, toCategory, fieldName);
+            WriteObject(rc);
+        }
+    }
+}
