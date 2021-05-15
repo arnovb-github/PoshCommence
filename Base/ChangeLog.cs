60633f8a1da7a64ac281089113df901d5c97c683
Arno van Boven <arnovanboven@outlook.com>
2021-05-15 12:49:26 +0200

    Add Clear--CmcControlCharacters

---
+++ Base/ChangeLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PoshCommence.Base
+{
+    internal class ChangeLog
+    {
+        internal string DatabaseName { get; set; }
+        internal string CategoryName { get; set; }
+
+        internal DateTime Date { get; set; }
+
+        readonly Dictionary<string, IEnumerable<FieldModification>> modifiedRows =
+            new Dictionary<string, IEnumerable<FieldModification>>();
+
+        internal Dictionary<string, IEnumerable<FieldModification>> ModifiedRows => modifiedRows;
+    }
+
+    internal class FieldModification
+    {
+        internal FieldModification(string itemName, string fieldName, string oldValue, string newValue)
+        {
+            ItemName = itemName;
+            FieldName = fieldName;
+            OldValue = oldValue;
+            NewValue = newValue;
+        }
+
+        public string ItemName { get; }
+        internal string FieldName { get; }
+        internal string OldValue { get; }
+        internal string NewValue { get; }
+    }
+}
